574addaeae4ebaa0ed587b96555a9d4878ec788a
Holt Walborn <hwalborn@gmail.com>
2017-01-12 20:57:39 +0000

    C'est Tu

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  name_hash = {}
+  data.each {|description, attribute|
+    attribute.each {|k, v|
+      v.each {|name|
+        if !(name_hash.has_key?(name))
+          name_hash[name] = {}
+        end
+        if name_hash[name].has_key?(description)
+          name_hash[name][description] << k.to_s
+        else
+          name_hash[name][description] = []
+          name_hash[name][description] << k.to_s
+        end
+      }
+    }
+  }
+  name_hash
 end
